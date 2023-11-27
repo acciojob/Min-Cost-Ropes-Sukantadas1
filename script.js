52f9c1ad17282d333828d8ddde2fd21fd4716f0f
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-27 17:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,72 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	 const heap = new MinHeap();
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(value) {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    pop() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+
+        const root = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return root;
+    }
+
+    heapifyUp() {
+        let currentIndex = this.heap.length - 1;
+
+        while (currentIndex > 0) {
+            const parentIndex = Math.floor((currentIndex - 1) / 2);
+
+            if (this.heap[currentIndex] < this.heap[parentIndex]) {
+                [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+                currentIndex = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    heapifyDown() {
+        let currentIndex = 0;
+
+        while (true) {
+            const leftChildIndex = 2 * currentIndex + 1;
+            const rightChildIndex = 2 * currentIndex + 2;
+            let smallestChildIndex = null;
+
+            if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[currentIndex]) {
+                smallestChildIndex = leftChildIndex;
+            } else {
+                smallestChildIndex = currentIndex;
+            }
+
+            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallestChildIndex]) {
+                smallestChildIndex = rightChildIndex;
+            }
+
+            if (smallestChildIndex !== currentIndex) {
+                [this.heap[currentIndex], this.heap[smallestChildIndex]] = [this.heap[smallestChildIndex], this.heap[currentIndex]];
+                currentIndex = smallestChildIndex;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+function mincost(arr) {
+    const heap = new MinHeap();
 
     // Insert all ropes into the min heap
     for (let i = 0; i < arr.length; i++) {
@@ -23,7 +87,6 @@ function mincost(arr)
     }
 
     return totalCost;
-  
 }
 
 module.exports=mincost;
