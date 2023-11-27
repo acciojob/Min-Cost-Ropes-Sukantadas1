74bfc502c8fa1b7c8164e0d071cb3cb0ea7860c8
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-27 17:37:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	 const heap = new MinHeap();
+
+    // Insert all ropes into the min heap
+    for (let i = 0; i < arr.length; i++) {
+        heap.push(arr[i]);
+    }
+
+    let totalCost = 0;
+
+    // Combine ropes until there is only one rope left in the heap
+    while (heap.heap.length > 1) {
+        const rope1 = heap.pop();
+        const rope2 = heap.pop();
+
+        const currentCost = rope1 + rope2;
+        totalCost += currentCost;
+
+        heap.push(currentCost);
+    }
+
+    return totalCost;
   
 }
 
